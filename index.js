443886b61edf09362d6a48c6638fec848d93d981
Jared McMahan <jaredm788@gmail.com>
2022-08-16 08:24:39 -0600

    commit tree e8d89c4c3334501a14cde4bc09714998bd6e6eed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+function saturdayFun(saturdayActivity = "roller-skate") {
+    return `This Saturday, I want to ${saturdayActivity}!`
+}
+
+function mondayWork (mondayActivity = 'go to the office') {
+    return `This Monday, I will ${mondayActivity}.`
+}
+
+// first try
+// function wrapAdjective(flair = "*") {
+//    const adj = "special";
+//    return `You are ${flair} ${adj} ${flair}!`
+// }
+
+function wrapAdjective(flair = "*") {
+   return function (adjWord = "special") {
+    return `You are ${flair}${adjWord}${flair}!`
+   }
+}
+
+wrapAdjective("%")("a dedicated programmer")
